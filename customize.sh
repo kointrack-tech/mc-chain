528279b442ce509c7250b25170f2ebf4458a71fb
Salesmask Techsystems <badrinath.mutkule@gmail.com>
2024-02-15 18:35:19 +0530

    customize

---
+++ customize.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cp customise/android-chrome-192x192.png blockscout/apps/block_scout_web/assets/static/android-chrome-192x192.png
+cp customise/android-chrome-512x512.png blockscout/apps/block_scout_web/assets/static/android-chrome-512x512.png
+cp customise/apple-touch-icon.png blockscout/apps/block_scout_web/assets/static/apple-touch-icon.png
+cp customise/icon.png blockscout/apps/block_scout_web/assets/static/mstile-150x150.png
+cp customise/logo.png blockscout/apps/block_scout_web/assets/static/images/logo.png
+
+cp customise/favicon.ico blockscout/apps/block_scout_web/assets/static/images/favicon.ico
+cp customise/favicon-32x32.png blockscout/apps/block_scout_web/assets/static/images/favicon-32x32.png
+cp customise/favicon-16x16.png blockscout/apps/block_scout_web/assets/static/images/favicon-16x16.png
+
+cp customise/_footer.html.eex blockscout/apps/block_scout_web/lib/block_scout_web/templates/layout/_footer.html.eex
